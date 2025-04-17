6c53efbc6587d42ecdfa663c38df727327945d71
anumarif833 <anumarif833@gmail.com>
2025-04-17 16:44:43 +0500

    add rontend osted link

--- backend/index.mjs
+++ backend/index.mjs
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ['http://localhost:5174', 'http://localhost:5173'],
+		origin: ['http://localhost:5174', 'http://localhost:5173',"https://mer-nstack-batch11-section-j-eta.vercel.app"],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
 		credentials: true,
 		allowedHeaders: ['Content-Type', 'Authorization'],
